3a27578f2c1d9635f823feb56103f95a088a8465
louis mahe <lmahe@made-f0Ar11s10.clusters.42paris.fr>
2024-01-16 14:10:26 +0100

    signalhandler2

---
+++ srcs/signal_handler2.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signal_handler2.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmahe <lmahe@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/16 12:18:23 by lmahe             #+#    #+#             */
+/*   Updated: 2024/01/16 14:02:37 by lmahe            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+#include <libft.h>
+#include <parse.h>
+#include <signal.h>
+
+extern int	g_mode;
+
+void	heredoc_handler(int sig)
+{
+	(void)sig;
+	g_mode = 1;
+}
+
+int	rec_heredoc_signal(void)
+{
+	struct sigaction	action_sa;
+
+	ft_bzero(&action_sa, sizeof(struct sigaction));
+	action_sa.sa_handler = heredoc_handler;
+	sigaction(SIGINT, &action_sa, NULL);
+	return (0);
+}
